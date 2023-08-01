89c8217bdbbbe09181283ae0c85e2ce87f8a9163
azizhajlaoui <azizhajlaoui2@gmail.com>
2023-08-01 10:05:44 +0100

    creation categories comp

---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+function Categories() {
+    return (
+        <div>
+            <select>
+                <option id="classique">classique</option>
+                <option id="extérieur">extérieur</option>
+                <option id="plante grasse">plante grasse</option>
+            </select>
+        </div>
+    )
+}
+
+export default Categories
