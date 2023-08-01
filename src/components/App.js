554becea6b63279fb60e666aa12f47405b845de2
azizhajlaoui <azizhajlaoui2@gmail.com>
2023-08-01 11:12:08 +0100

    cache

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,27 @@ import logo from '../assets/logo.png'
 import Cart from './cart'
 import Footer from './footer'
 import ShoppingList from './ShoppingList'
+import Time from './Time.js'
 import '../styles/Layout.css'
 
 function App() {
 	const [cart, updateCart] = useState([])
+	const [isFooterShown, updateIsFooterShown] = useState(true)
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
+				<Time />
 			</Banner>
 			<div className='lmj-layout-inner'>
 				<Cart cart={cart} updateCart={updateCart} />
 				<ShoppingList cart={cart} updateCart={updateCart} />
+				<button onClick={() => updateIsFooterShown(!isFooterShown)}>
+					Cacher !
+				</button>
 			</div>
-			<Footer />
+			{isFooterShown && <Footer cart={cart} />}
 		</div>
 	)
 }
